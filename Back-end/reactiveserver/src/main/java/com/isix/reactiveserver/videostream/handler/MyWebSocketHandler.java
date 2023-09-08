fdd2d9fed5371322232a747ca6375af7bc93f773
이강민 <ygangmin@gmail.com>
2023-09-08 16:18:38 +0900

    [BE] Feat : SocketHandler 수정

--- Back-end/reactiveserver/src/main/java/com/isix/reactiveserver/videostream/handler/MyWebSocketHandler.java
+++ Back-end/reactiveserver/src/main/java/com/isix/reactiveserver/videostream/handler/MyWebSocketHandler.java
@@ -1,40 +1,67 @@
 package com.isix.reactiveserver.videostream.handler;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.WebSocketMessage;
-import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 
-public class MyWebSocketHandler implements WebSocketHandler {
-    /*@Override
-    public Mono<Void> handle(WebSocketSession session) {
-        Flux<WebSocketMessage> input = session.receive()
-                .doOnNext(message -> {
-                    // Handle incoming messages from the client
-                    String payload = message.getPayloadAsText();
-                    // Process and respond to the client as needed
-                });
+@Component
+public class MyWebSocketHandler extends TextWebSocketHandler {
+    private int set = 0;
+    private Socket socket;
 
-        Flux<WebSocketMessage> output = input;// Create a Flux for sending data to the client
+    public MyWebSocketHandler() throws IOException {
+        socket = new Socket("127.0.0.1",8888);
+    }
 
-        return session.send(output);
-    }*/
     @Override
-    public Mono<Void> handle(WebSocketSession session) {
-        return session
-                .receive()
-                .doOnNext(message -> {
-                    // Handle incoming messages from the client
-                    String payload = message.getPayloadAsText();
-                    // Process and respond to the client as needed
-                })
-                .doOnError(error -> {
-                    // Handle WebSocket errors here
-                    System.err.println("WebSocket error: " + error.getMessage());
-                })
-                .then();
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        if (message.getPayload() instanceof ByteBuffer payloadBuffer) {
+
+            // Convert the ByteBuffer to a byte array
+            byte[] payloadBytes = new byte[payloadBuffer.remaining()];
+            payloadBuffer.get(payloadBytes);
+
+            // Create a new ByteBuffer to send to another socket (assuming you have another WebSocketSession)
+            ByteBuffer newPayloadBuffer = ByteBuffer.wrap(payloadBytes);
+
+            // Send the ByteBuffer as a WebSocket message
+            WebSocketMessage<ByteBuffer> newMessage = new BinaryMessage(newPayloadBuffer);
+            session.sendMessage(newMessage);
+            socket.getOutputStream().write(payloadBytes);
+
+            //System.out.println("Sent ByteBuffer message to another socket.");
+        } else {
+            // Handle other types of payloads (e.g., TextMessage)
+            String payload = message.toString();
+            session.sendMessage(new TextMessage(payload));
+            System.out.println(message.getPayload().getClass());
+        }
+    }
+
+    /*@Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        String payload = message.toString();
+        *//*System.out.println("???");
+        System.out.println(payload);*//*
+        set ++;
+        System.out.println(set);
+    }*/
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+
+        // Set the desired buffer size for the WebSocket session
+        int bufferSize = 100000; // Adjust the buffer size as needed
+        session.setTextMessageSizeLimit(bufferSize);
+        session.setBinaryMessageSizeLimit(bufferSize);
+
     }
 }
