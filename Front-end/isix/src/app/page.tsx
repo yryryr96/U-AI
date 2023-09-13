658cafe742614cd5eee246f3bd0bb6b3d28d5483
정훈석 <wjdgnstjr45@naver.com>
2023-09-13 10:48:00 +0900

    [FE] Feat : Landing Page Initialize 기본코드 작성

--- Front-end/isix/src/app/page.tsx
+++ Front-end/isix/src/app/page.tsx
@@ -1,5 +1,5 @@
-import MainPage from "@/pageComponents/mainpage";
+import LandingPage from '@/pageComponents/landing'
 
 export default function Home() {
-  return <MainPage></MainPage>;
+  return <LandingPage />;
 }
