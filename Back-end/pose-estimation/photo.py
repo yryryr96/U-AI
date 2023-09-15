ed8dc9c96ffb18c4f998e00e26c396b548bd6750
이강민 <ygangmin@gmail.com>
2023-09-15 14:33:25 +0900

    [BE] Feat : 사진 크롭 (검출된 객체 Object boundary 기준)

--- Back-end/pose-estimation/photo.py
+++ Back-end/pose-estimation/photo.py
@@ -12,14 +12,37 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # Define path to the image file
-source = '2567-6-21.jpg'
+source = './image/test5.jpg'
+
+input_image = cv2.imread(source)
 
 # Run inference on the source
-results = model.predict(source, classes=[0, 1])  # list of Results objects
+results = model.predict(input_image, classes=[0, 1])  # list of Results objects
 
 annotated_frame = results[0].plot()
 
-cv2.imshow("YOLOv8 Inference", annotated_frame)
+# rectangles = []
+
+height, width, _ = input_image.shape
+output_image = np.ones_like(input_image) * 255
+
+for xys in results[0].boxes.xyxy:
+    print(xys)
+
+    x_min, y_min, x_max, y_max = xys.cpu().numpy()
+
+    x_min = round(x_min)
+    y_min = round(y_min)
+    y_max = round(y_max)
+    x_max = round(x_max)
+
+
+    print(x_min, y_min, x_max, y_max)
+
+    output_image[y_min:y_max, x_min:x_max] = input_image[y_min:y_max, x_min:x_max]
+
+
+cv2.imshow("YOLOv8 Inference", output_image)
 print(results)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
