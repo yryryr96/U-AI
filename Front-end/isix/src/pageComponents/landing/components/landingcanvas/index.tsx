091c7fffe0e196fd1ca37fa9046f18094b4b6682
정훈석 <wjdgnstjr45@naver.com>
2023-09-15 18:01:16 +0900

    [FE] Fix : 랜딩페이지 캐릭터 위치 수정

--- Front-end/isix/src/pageComponents/landing/components/landingcanvas/index.tsx
+++ Front-end/isix/src/pageComponents/landing/components/landingcanvas/index.tsx
@@ -9,8 +9,8 @@ const LandingCanvas = () => {
   return (
     <Canvas orthographic camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 50 }}>
       <Scene />
-      <CharacterScene position={[60, 10, 0]} width={600} height={600} gifUrl='/resources/policepanda2.gif' />
-      <CharacterScene position={[-170, 20, 0]} width={700} height={700} gifUrl='/resources/teacherpanda2.gif' />
+      <CharacterScene position={[90, 70, 0]} width={600} height={600} gifUrl='/resources/policepanda2.gif' />
+      <CharacterScene position={[-200, 80, 0]} width={700} height={700} gifUrl='/resources/teacherpanda2.gif' />
       <Effects />
     </Canvas>
   );
