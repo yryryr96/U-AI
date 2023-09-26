4d7542f4ff6f939f394c58ac9d8f7f0514d0208e
전대현 <wjseogus523@naver.com>
2023-09-26 15:04:15 +0900

    [FE] Fix : 추가 스타일 변경 및 배경

--- Front-end/isix/src/pageComponents/fire/components/story/Story.styled.tsx
+++ Front-end/isix/src/pageComponents/fire/components/story/Story.styled.tsx
@@ -110,18 +110,6 @@ export const StyledTimer = styled.div`
   text-align: center;
 `
 
-export const StyledLeft = styled.div`
-  font-size: 13rem;
-  font-weight: bold;
-  color: lightgreen;
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  top : -0.5vw;
-  right : 8vw;
-  text-align: center;
-`
-
 export const StyledRight = styled.div`
   font-size: 13rem;
   font-weight: bold;
@@ -143,18 +131,6 @@ export const StyledLeft = styled.div`
   width: 100%;
   top : -0.5vw;
   right : 8vw;
-  text-align: center;
-`
-
-export const StyledRight = styled.div`
-  font-size: 13rem;
-  font-weight: bold;
-  color: lightgreen;
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  top : -0.5vw;
-  left : 8vw;
   text-align: center;
 `
 
