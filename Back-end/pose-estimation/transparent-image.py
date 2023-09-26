60d16c35c91e0bc2a493246d5fbe61f541e2005f
이강민 <ygangmin@gmail.com>
2023-09-26 17:33:03 +0900

    [BE] Feat : 이미지 투명화, webp로 만들기

---
+++ Back-end/pose-estimation/transparent-image.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+
+
+def process_images(src_folder, dest_folder, target_rgb, tolerance=60):
+    for filename in os.listdir(src_folder):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            img = Image.open(os.path.join(src_folder, filename)).convert("RGBA")
+            datas = img.getdata()
+
+            newData = []
+            for item in datas:
+                # change all white (also shades of whites)
+                # pixels to yellow
+                if all([abs(item[i] - target_rgb[i]) <= tolerance for i in range(3)]):
+                    newData.append((255, 255, 255, 0))  # make it transparent (also shades of transparency)
+                else:
+                    newData.append(item)
+
+            img.putdata(newData)
+
+            if not os.path.exists(dest_folder):
+                os.makedirs(dest_folder)
+
+            img.save(os.path.join(dest_folder, filename), "PNG")
+
+
+# usage example
+process_images('C:/Users/SSAFY/Desktop/ㅍ0ㅐㄴ더/processed', 'C:/Users/SSAFY/Desktop/ㅍ0ㅐㄴ더/processed/trans', (32, 173, 67))
