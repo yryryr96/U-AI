b491e70095466d75d246b1a7c1bf74f76953bbd6
DryLeonhard <ygangmin@gmail.com>
2023-10-03 04:38:00 +0900

    [BE] Feat : Spring 서버에 이미지 리스트, 이미지 요청 djanog서버에서 받아올 수 있도록

--- Back-end/reactiveserver/src/main/java/com/isix/reactiveserver/file/controller/FileController.java
+++ Back-end/reactiveserver/src/main/java/com/isix/reactiveserver/file/controller/FileController.java
@@ -2,19 +2,18 @@ package com.isix.reactiveserver.file.controller;
 
 import com.isix.reactiveserver.exception.BusinessLogicException;
 import com.isix.reactiveserver.exception.ExceptionCode;
+import com.isix.reactiveserver.file.service.FileService;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,12 +21,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@RequiredArgsConstructor
 public class FileController {
 
-    @Value("${app.savedimage.rootpath}")
-    String folderRoot;
+    private final FileService fileService;
+
+    /*@Value("${app.savedimage.rootpath}")
+    String folderRoot;*/
 
     @GetMapping("/image/{sessionId}")
+    public ResponseEntity<String[]> getImageList(@PathVariable String sessionId){
+        return new ResponseEntity<>(fileService.getImageList(sessionId),HttpStatus.OK);
+    }
+
+    @GetMapping("/image/{sessionId}/{name}")
+    public void download(@PathVariable String name, @PathVariable String sessionId, HttpServletResponse response)throws Exception{
+
+        byte[] fileBytes = fileService.getImage(sessionId,name);
+
+        response.setHeader("Content-Disposition","attachment;filename=" + name);
+        try(ByteArrayInputStream fileInputStream = new ByteArrayInputStream(fileBytes)){
+            OutputStream out = response.getOutputStream();
+
+            int read = 0;
+            byte[] buffer = new byte[1024];
+            while ((read = fileInputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new Exception("Download Error");
+        }
+    }
+
+
+    /*@GetMapping("/image/{sessionId}")
     public ResponseEntity<String[]> getImageList(@PathVariable String sessionId){
 
         File directory = new File(folderRoot + File.separator + sessionId);
@@ -55,6 +84,6 @@ public class FileController {
             e.printStackTrace();
             throw new Exception("Download Error");
         }
-    }
+    }*/
 }
 
