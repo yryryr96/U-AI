09bc251ad0d28d8b9f8c6732369ae8f4d7b7cfe5
Andrea <wjdgnstjr45@gmail.com>
2023-10-04 00:25:59 +0900

    [FE] Fix : 카운트다운 추가

--- Front-end/isix/src/commonComponents/story/recordComponent/index.tsx
+++ Front-end/isix/src/commonComponents/story/recordComponent/index.tsx
@@ -1,18 +1,42 @@
 import { useEffect, useRef, useState } from 'react';
 import axios, { AxiosResponse } from "axios";
+import { position } from 'html2canvas/dist/types/css/property-descriptors/position';
+import styled from 'styled-components';
 
 interface RecordComponentProps {
   onResult: (result: number) => void;
 }
 
+export const StyledTimer = styled.div`
+  font-size: 20rem;
+  font-weight: bold;
+  color: red;
+  z-index: 2;
+  position: absolute;
+  width: 100%;
+  top: 10vh;
+  text-align: center;
+`
+
 const RecordComponent = ({ onResult }: RecordComponentProps): JSX.Element => {
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const [retryKey, setRetryKey] = useState(0);
   const sessionId = localStorage.getItem('socketId') || '';
+  const [countdown, setCountdown] = useState(0);
 
   useEffect(() => {
     console.log('id: ', sessionId)
     const startRecording = async () => {
+      setTimeout(() => {
+        setCountdown(3)
+        let countdownIntervalId = setInterval(() => {
+          setCountdown(prevCountdown => prevCountdown -1);
+        },1000);
+
+        setTimeout(() => clearInterval(countdownIntervalId),3000);
+
+      }, 3500);
+      
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         const mediaRecorder: MediaRecorder = new MediaRecorder(stream);
@@ -58,16 +82,18 @@ const RecordComponent = ({ onResult }: RecordComponentProps): JSX.Element => {
          }
 
         setTimeout(() => {
-          mediaRecorder.start();
-          console.log('start');
-        }, 2000);
+          if(countdown === 0){ 
+            mediaRecorder.start();
+            console.log('start');
+          }
+        },3000);
 
         setTimeout(() => {
           if(mediaRecorder.state === 'recording'){
             mediaRecorder.stop();
             console.log('end');
           }
-        }, 7000); 
+        }, 3000); 
 
       } catch (err) {
       	console.error(err)
@@ -87,7 +113,7 @@ const RecordComponent = ({ onResult }: RecordComponentProps): JSX.Element => {
 }, [retryKey]);
 
 return (
-<></>
+<>{countdown > 0 && <StyledTimer>{countdown}</StyledTimer>}</>
 )
 }
 
