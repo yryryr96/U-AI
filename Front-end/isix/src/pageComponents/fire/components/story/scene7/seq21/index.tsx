14b753f061ba1ffeffe63ebf80135ab1edc81573
정훈석 <wjdgnstjr45@naver.com>
2023-10-04 12:47:38 +0900

    [FE] Fix : 스테이트 변경 시간 조정, 일부 경로 수정

--- Front-end/isix/src/pageComponents/fire/components/story/scene7/seq21/index.tsx
+++ Front-end/isix/src/pageComponents/fire/components/story/scene7/seq21/index.tsx
@@ -38,7 +38,9 @@ const Seq21: React.FC<WebcamProps> = ({ startStream, stopStream, videoElm, hidde
         setRight(response.data.right)
         if (timer === 0) {
           if (response.data.left < response.data.right) {
-            setState(state + 1)
+            setTimeout(() => {
+                setState(state + 1);
+            }, 500);
           } else {
             setAudioUrl('/resources/audioFile/incorrect.mp3');
             setTimer(10)
