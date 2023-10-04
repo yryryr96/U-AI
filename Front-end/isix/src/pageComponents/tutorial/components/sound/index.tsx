510234288f4f91c6a6b4b32e1748f698678eedb4
전대현 <wjseogus523@naver.com>
2023-10-05 01:30:01 +0900

    [FE] Feat : 기본 듀토리얼 완료

--- Front-end/isix/src/pageComponents/tutorial/components/sound/index.tsx
+++ Front-end/isix/src/pageComponents/tutorial/components/sound/index.tsx
@@ -1,16 +1,19 @@
 import TextComponent from "@/commonComponents/story/textComponent"
+import ImageComponent from "@/commonComponents/story/imageComponent"
+import AudioPlayer from "@/commonComponents/story/audioComponent"
 import { StyledTutorialContainer } from "../../Tutorial.styled"
 
 const Sound = () => {
     const text: string = `판다와 함께 크게 외쳐주세요!`
-    // const audioUrl: string = '/resources/audioFile/seq1.mp3'
+    const audioUrl: string = '/resources/audioFile/tutorialSTT.mp3'
     return (
       <>  
         <StyledTutorialContainer>
+          <ImageComponent src='/resources/images/speak_panda2.png' width={1000} height={900} style={{marginTop : "15vh",marginRight : "10vw"}} />
         </StyledTutorialContainer>
-
+       
         <TextComponent text={text} style={{marginTop : '10vh', fontSize: '3.5rem' }} />
-        {/* <AudioPlayer file={audioUrl} /> */}
+        <AudioPlayer file={audioUrl} />
       </>
     )
 }
