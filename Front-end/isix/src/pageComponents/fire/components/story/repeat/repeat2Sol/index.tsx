45b37e82293ecb5b7e2a4acde1dfbb8230b1b61c
정영록 <s9285s@naver.com>
2023-10-04 21:52:41 +0900

    [FE] Feat : OCR 문제에서 몇 명이 정답인지 출력

--- Front-end/isix/src/pageComponents/fire/components/story/repeat/repeat2Sol/index.tsx
+++ Front-end/isix/src/pageComponents/fire/components/story/repeat/repeat2Sol/index.tsx
@@ -1,7 +1,8 @@
 import CamComponent from "@/commonComponents/story/camComponent"
-import { BorderHeight, BorderWidth, StyledBorders, StyledCamText, StyledStoryCam } from "../../Story.styled"
+import { BorderHeight, BorderWidth, StyledBorders, StyledCamText, StyledCenter, StyledStoryCam } from "../../Story.styled"
 import AudioPlayer from "@/commonComponents/story/audioComponent";
 import HomeButton from "@/commonComponents/story/homeButtonComponent";
+import useOcrCorrect from "@/stores/ocr/useOcrCorrect";
 
 interface WebcamProps {
   videoElm: JSX.Element;
@@ -13,6 +14,7 @@ interface WebcamProps {
 const Repeat2Sol: React.FC<WebcamProps> = ({ startStream, stopStream, videoElm, hiddenCanvasElm }) => {
   const text: string = '정답은 119입니다!'
   const audioUrl: string = '/resources/audioFile/repeat2Sol.mp3'
+  const {correct, setCorrect} = useOcrCorrect();
 
   return (
     <>
@@ -24,9 +26,10 @@ const Repeat2Sol: React.FC<WebcamProps> = ({ startStream, stopStream, videoElm, 
         <BorderWidth />
         <BorderWidth />
       </StyledBorders>
-
+      <StyledCenter>{correct}명 정답!!</StyledCenter>
       <StyledCamText>{text}</StyledCamText>
       <StyledStoryCam>
+      
         <CamComponent videoElm={videoElm} hiddenCanvasElm = { hiddenCanvasElm } startStream = {startStream} stopStream={stopStream} />
       </StyledStoryCam>
       <AudioPlayer file={audioUrl} />
